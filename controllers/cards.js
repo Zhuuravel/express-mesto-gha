97aaee7b76d273a25f617bcf0d791550ead28cac
Zhuuravel <zhuuravel@yandex.ru>
2023-12-12 18:41:20 +0300

    fix like card req

--- controllers/cards.js
+++ controllers/cards.js
@@ -41,25 +41,22 @@ module.exports.deleteCard = (req, res) => {
     });
 };
 
-module.exports.likeCard = (req, res) => {
-  const { cardId } = req.params;
-  Card.findByIdAndUpdate(
-    cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true },
-  ).then((card) => {
-    if (!card) {
-      return res.status(NOT_FOUND).send({ message: `Карточка с указанным id: ${cardId} не найдена` });
-    } return res.status(200).send(card);
-  })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные для постановки лайка' });
-      } else {
-        res.status(SERVER_ERROR).send({ message: 'Произошла ошибка на сервере' });
-      }
-    });
-};
+module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+  { new: true },
+).then((card) => {
+  if (!card) {
+    return res.status(NOT_FOUND).send({ message: `Карточка с указанным id: ${req.params.cardId} не найдена` });
+  } return res.status(200).send(card);
+})
+  .catch((err) => {
+    if (err.name === 'CastError') {
+      res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные для снятия лайка' });
+    } else {
+      res.status(SERVER_ERROR).send({ message: 'Произошла ошибка на сервере' });
+    }
+  });
 
 module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
