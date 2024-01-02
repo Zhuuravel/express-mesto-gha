0e96c3deda457a2394a27c8e41d4782e4435d52d
Zhuravel <zhuuravel@yandex.ru>
2024-01-02 22:44:55 +0300

    fix comment error

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const helmet = require('helmet');
 const routes = require('./routes');
 const NotFound = require('./errors/NotFound');
 const errorHandler = require('./middlewares/error-handler');
-// kuku
 const app = express();
-//comment
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
